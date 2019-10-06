9900956695c581a83925060cef68317e6f329fdb
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-07 00:31:21 +0300

    solve js-edu

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+module.exports = function getTimeForEducation(focus = 'family', knowsProgramming = true, config = {family: 4}) {
+      let hours;
+      if (knowsProgramming == true) {   
+        hours = 800; // if student has coding skill he needs time to learn only master js stage (800 hours)
+        } else { hours = 1300}; // otherwise student has to learn full course: basic and master stages (500 + 800 hours)
+
+      let weeks = Math.ceil(hours / config[focus]);
+
+      return weeks;
+      
   };
+  
+  
   
